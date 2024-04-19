3b8e59055e4d0ef22828b3ec4f3884318f5462d4
Subhransu Mishra <subhransu.mishra@sap.com>
2024-04-19 13:12:04 +0530

    Initial Commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO notes
+    (body)
+VALUES
+    ('Ask Larry about the TPS reports');
+
+INSERT INTO notes
+    (body)
+VALUES
+    ('Ask Larry about the TPS reports 2');
+
+INSERT INTO notes
+    (body)
+VALUES
+    ('Ask Larry about the TPS reports 3');
