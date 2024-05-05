c352ba87bd27c15342c9243757bcdc03ccdc02f8
Subhransu Mishra <subhransu.mishra@sap.com>
2024-05-05 22:29:37 +0530

    Updated with new table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,18 @@ INSERT INTO notes
     (body)
 VALUES
     ('Ask Larry about the TPS reports 3');
+
+INSERT INTO author
+    (name)
+VALUES
+    ('Author of 1');
+
+INSERT INTO author
+    (name)
+VALUES
+    ('Author of 2');
+
+INSERT INTO author
+    (name)
+VALUES
+    ('Author of 3');
